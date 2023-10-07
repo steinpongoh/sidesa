81c5cff7a1db346cfabf4da58eb711939489e439
trick2001 <willemlouis99@gmail.com>
2023-10-07 21:21:09 +0800

    mengubah komen

--- templates/dbconnect.php
+++ templates/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 $dbconnect = mysqli_connect("localhost", "root", "", "desa");
 
-// Cek apakah ada error di database koneksi
+// Cek apakah ada error di database
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
