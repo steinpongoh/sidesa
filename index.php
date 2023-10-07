606b8d30423d741adaf9cdc0745628ff176c68c5
trick2001 <willemlouis99@gmail.com>
2023-10-07 23:34:46 +0800

    melengkapi responsive web

--- index.php
+++ index.php
@@ -177,6 +177,7 @@ $jumlahWanita = count(query('SELECT * FROM penduduk WHERE jenis_kelamin = "Wanit
       </div>
     </div>
   </section>
+  <?php include 'templates/footer.php'; ?>
   <!-- Statistik Kependudukan -->
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
---
+++ templates/footer.php
@@ -0,0 +1,13 @@
+<?php
+include 'header.php';
+?>
+
+<section id="footer" class="footer">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 text-center">
+                <h1>Footer</h1>
+            </div>
+        </div>
+    </div>
+</section>
