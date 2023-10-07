f371b36c18f04301400f2b7dd1ef912daec2d957
trick2001 <willemlouis99@gmail.com>
2023-10-07 23:57:00 +0800

    mengubah navbar

--- index.php
+++ index.php
@@ -7,175 +7,172 @@ $jumlahPria = count(query('SELECT * FROM penduduk WHERE jenis_kelamin = "Pria"')
 $jumlahWanita = count(query('SELECT * FROM penduduk WHERE jenis_kelamin = "Wanita"'))
 ?>
 
-  <!-- Home -->
-  <section id="home" class="home w-100 h-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white">
-    <main>
-      <h3 class="animate__animated animate__slideInLeft">SISTEM INFORMASI</h3>
-      <h1 class="animate__animated animate__slideInRight">DESA TONDANGOW</h1>
-    </main>
-  </section>
-  <!-- Home -->
+<!-- Home -->
+<section id="home" class="home w-100 h-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white">
+  <main>
+    <h3 class="animate__animated animate__slideInLeft">SISTEM INFORMASI</h3>
+    <h1 class="animate__animated animate__slideInRight">DESA TONDANGOW</h1>
+  </main>
+</section>
+<!-- Home -->
 
-  <!-- Struktur Pemerintah -->
-  <section id="strukturpemerintah" class="strukturpemerintah">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8 text-center">
-          <h2>Struktur Pemerintahan</h2>
-          <div class="col-4">
-            <img src="img/struktur.png" alt="" class="img-responsive" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- Struktur Pemerintah -->
-
-  <!-- Visi & Misi Desa -->
-  <section id="visimisi" class="visimisi">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8 text-center">
-          <h2>Visi & Misi Desa</h2>
-          <h4>Visi</h4>
-          <p>
-            "Menjadi desa yang sejahtera, berkelanjutan, dan berbudaya, di
-            mana masyarakat hidup harmonis, berdaya, dan memiliki akses penuh
-            terhadap pendidikan, kesehatan, serta sumber daya yang
-            berkelanjutan."
-          </p>
-          <h4>Misi</h4>
-          <ol class="text-start">
-            <li>
-              <p class="fw-semibold">Pengembangan Ekonomi dan Kesejahteraan:</p>
-              <p>
-                Mendorong pertumbuhan ekonomi inklusif dan berkelanjutan melalui pembangunan infrastruktur, pelatihan keterampilan, dan promosi usaha mikro, kecil, dan menengah.
-              </p>
-            </li>
-
-            <li>
-              <p class="fw-semibold">Pendidikan dan Pengetahuan:</p>
-              <p>
-                Menyediakan akses pendidikan berkualitas, memfasilitasi pelatihan keterampilan, dan mendorong literasi untuk meningkatkan pengetahuan dan keterampilan masyarakat.
-              </p>
-            </li>
-
-            <li>
-              <p class="fw-semibold">Kesehatan dan Kesejahteraan:</p>
-              <p>
-                Memastikan akses penuh terhadap layanan kesehatan, penyuluhan kesehatan, vaksinasi, dan infrastruktur sanitasi untuk meningkatkan kesehatan dan kesejahteraan masyarakat.
-              </p>
-            </li>
-
-            <li>
-              <p class="fw-semibold">Lingkungan Hidup yang Berkelanjutan:</p>
-              <p>
-                Memelihara dan melindungi lingkungan alam desa, mengelola sampah secara efektif, dan mempromosikan tindakan berkelanjutan untuk menjaga keindahan dan kelestarian alam.
-              </p>
-            </li>
-
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- Visi & Misi Desa -->
-
-  <!-- Sejarah Desa -->
-  <section id="sejarahdesa" class="sejarahdesa">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-7">
-          <h2>Sejarah Desa</h2>
-          <p>Desa Tondangow adalah sebuah permukiman kecil yang didirikan pada abad ke-17 oleh sekelompok petani yang bermigrasi mencari tanah subur dan tempat aman. Mereka membangun desa di sekitar sumber air yang melimpah.</p>
-          <p>Pada abad ke-18 dan ke-19, Desa Tondangow tumbuh pesat berkat pertumbuhan pertanian dan perdagangan. Tanah subur dan iklim yang baik memungkinkan desa ini memproduksi hasil pertanian yang dicari di pasar regional, menjadikannya pusat perdagangan penting.</p>
-          <p>Selama abad ke-20, Desa Tondangow memainkan peran kunci dalam perjuangan kemerdekaan Nama Negara. Penduduk desa secara aktif terlibat dalam gerakan kemerdekaan melawan penjajah, menandai sejarah perjuangan dan patriotisme desa ini.</p>
-        </div>
-        <div class="col-lg-5">
-          <img src="img/sejarahdesa.jpg" alt="" class="img-responsive" />
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- Sejarah Desa -->
-
-  <!-- Tata Tertib Desa -->
-  <section id="tatatertib" class="tatatertib">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8 text-center">
-          <h2>Tata Tertib Desa</h2>
-          <ol class="text-start">
-            <li>
-              <h5 class="fw-semibold">Kesopanan dan Etika:</h5>
-              <ul>
-                <li>Menghormati sesama warga desa dan tetangga.</li>
-                <li>Menjaga tutur kata yang sopan dan menghindari kata-kata kasar.</li>
-                <li>Menghormati kebudayaan dan adat istiadat desa.</li>
-              </ul>
-            </li>
-            <li>
-              <h5 class="fw-semibold">Pembinaan Masyarakat:</h5>
-              <ul>
-                <li>Mengikuti kegiatan pembinaan dan penyuluhan yang diselenggarakan oleh pemerintah desa.</li>
-                <li>Berpartisipasi dalam kegiatan sosial, keagamaan, dan budaya yang dapat mempererat hubungan antarwarga desa.</li>
-              </ul>
-            </li>
-            <li>
-              <h5 class="fw-semibold">Pelayanan Publik:</h5>
-              <ul>
-                <li>Menghormati petugas pelayanan publik dan mengajukan permohonan pelayanan dengan benar.</li>
-                <li>Memberikan umpan balik terhadap pelayanan yang diberikan untuk perbaikan di masa mendatang.</li>
-              </ul>
-            </li>
-            <li>
-              <h5 class="fw-semibold">Keamanan dan Ketertiban</h5>
-              <ul>
-                <li>Menghindari tindakan kriminal, kekerasan, dan perusakan harta benda.</li>
-                <li>Melaporkan kegiatan yang mencurigakan atau tindakan yang mengganggu ketertiban desa kepada pihak berwenang.</li>
-              </ul>
-            </li>
-            <li>
-              <h5 class="fw-semibold">Pengelolaan Lingkungan</h5>
-              <ul>
-                <li>Melakukan kegiatan penanaman pohon dan menjaga kebersihan lingkungan.</li>
-                <li>Menghindari tindakan yang dapat merusak lingkungan seperti illegal logging dan penangkapan ikan ilegal.</li>
-              </ul>
-            </li>
-          </ol>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- Tata Tertib Desa -->
-
-  <!-- Statistik Kependudukan -->
-  <section id="statistik" class="statistik">
+<!-- Struktur Pemerintah -->
+<section id="strukturpemerintah" class="strukturpemerintah">
+  <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-8 text-center">
-        <h2>Statistik Kependudukan</h2>
-        <div class="col-lg-6 d-flex justify-content-between align-items-center w-100 h-100">
-          <span><i class="bi bi-people"></i><br />
-            <h2><?php echo $jumlahPenduduk; ?></h2>
-            <h5>Jumlah Penduduk</h5>
-          </span>
-          <span><i class="bi bi-person-vcard"></i><br />
-            <h2><?php echo $jumlahKepalaKeluarga ?></h2>
-            <h5>Kepala Keluarga</h5>
-          </span>
-          <span><i class="bi bi-gender-male"></i><br />
-            <h2><?php echo $jumlahPria ?></h2>
-            <h5>Laki-Laki</h5>
-          </span>
-          <span><i class="bi bi-gender-female"></i><br />
-            <h2><?php echo $jumlahWanita ?></h2>
-            <h5>Perempuan</h5>
-          </span>
+        <h2>Struktur Pemerintahan</h2>
+        <div class="col-4">
+          <img src="img/struktur.png" alt="" class="img-responsive" />
         </div>
       </div>
     </div>
-  </section>
-  <?php include 'templates/footer.php'; ?>
-  <!-- Statistik Kependudukan -->
-<?php include 'templates/footer.php';?>
+  </div>
+</section>
+<!-- Struktur Pemerintah -->
 
+<!-- Visi & Misi Desa -->
+<section id="visimisi" class="visimisi">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8 text-center">
+        <h2>Visi & Misi Desa</h2>
+        <h4>Visi</h4>
+        <p>
+          "Menjadi desa yang sejahtera, berkelanjutan, dan berbudaya, di
+          mana masyarakat hidup harmonis, berdaya, dan memiliki akses penuh
+          terhadap pendidikan, kesehatan, serta sumber daya yang
+          berkelanjutan."
+        </p>
+        <h4>Misi</h4>
+        <ol class="text-start">
+          <li>
+            <p class="fw-semibold">Pengembangan Ekonomi dan Kesejahteraan:</p>
+            <p>
+              Mendorong pertumbuhan ekonomi inklusif dan berkelanjutan melalui pembangunan infrastruktur, pelatihan keterampilan, dan promosi usaha mikro, kecil, dan menengah.
+            </p>
+          </li>
 
+          <li>
+            <p class="fw-semibold">Pendidikan dan Pengetahuan:</p>
+            <p>
+              Menyediakan akses pendidikan berkualitas, memfasilitasi pelatihan keterampilan, dan mendorong literasi untuk meningkatkan pengetahuan dan keterampilan masyarakat.
+            </p>
+          </li>
+
+          <li>
+            <p class="fw-semibold">Kesehatan dan Kesejahteraan:</p>
+            <p>
+              Memastikan akses penuh terhadap layanan kesehatan, penyuluhan kesehatan, vaksinasi, dan infrastruktur sanitasi untuk meningkatkan kesehatan dan kesejahteraan masyarakat.
+            </p>
+          </li>
+
+          <li>
+            <p class="fw-semibold">Lingkungan Hidup yang Berkelanjutan:</p>
+            <p>
+              Memelihara dan melindungi lingkungan alam desa, mengelola sampah secara efektif, dan mempromosikan tindakan berkelanjutan untuk menjaga keindahan dan kelestarian alam.
+            </p>
+          </li>
+
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Visi & Misi Desa -->
+
+<!-- Sejarah Desa -->
+<section id="sejarahdesa" class="sejarahdesa">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-7">
+        <h2>Sejarah Desa</h2>
+        <p>Desa Tondangow adalah sebuah permukiman kecil yang didirikan pada abad ke-17 oleh sekelompok petani yang bermigrasi mencari tanah subur dan tempat aman. Mereka membangun desa di sekitar sumber air yang melimpah.</p>
+        <p>Pada abad ke-18 dan ke-19, Desa Tondangow tumbuh pesat berkat pertumbuhan pertanian dan perdagangan. Tanah subur dan iklim yang baik memungkinkan desa ini memproduksi hasil pertanian yang dicari di pasar regional, menjadikannya pusat perdagangan penting.</p>
+        <p>Selama abad ke-20, Desa Tondangow memainkan peran kunci dalam perjuangan kemerdekaan Nama Negara. Penduduk desa secara aktif terlibat dalam gerakan kemerdekaan melawan penjajah, menandai sejarah perjuangan dan patriotisme desa ini.</p>
+      </div>
+      <div class="col-lg-5">
+        <img src="img/sejarahdesa.jpg" alt="" class="img-responsive" />
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Sejarah Desa -->
+
+<!-- Tata Tertib Desa -->
+<section id="tatatertib" class="tatatertib">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8 text-center">
+        <h2>Tata Tertib Desa</h2>
+        <ol class="text-start">
+          <li>
+            <h5 class="fw-semibold">Kesopanan dan Etika:</h5>
+            <ul>
+              <li>Menghormati sesama warga desa dan tetangga.</li>
+              <li>Menjaga tutur kata yang sopan dan menghindari kata-kata kasar.</li>
+              <li>Menghormati kebudayaan dan adat istiadat desa.</li>
+            </ul>
+          </li>
+          <li>
+            <h5 class="fw-semibold">Pembinaan Masyarakat:</h5>
+            <ul>
+              <li>Mengikuti kegiatan pembinaan dan penyuluhan yang diselenggarakan oleh pemerintah desa.</li>
+              <li>Berpartisipasi dalam kegiatan sosial, keagamaan, dan budaya yang dapat mempererat hubungan antarwarga desa.</li>
+            </ul>
+          </li>
+          <li>
+            <h5 class="fw-semibold">Pelayanan Publik:</h5>
+            <ul>
+              <li>Menghormati petugas pelayanan publik dan mengajukan permohonan pelayanan dengan benar.</li>
+              <li>Memberikan umpan balik terhadap pelayanan yang diberikan untuk perbaikan di masa mendatang.</li>
+            </ul>
+          </li>
+          <li>
+            <h5 class="fw-semibold">Keamanan dan Ketertiban</h5>
+            <ul>
+              <li>Menghindari tindakan kriminal, kekerasan, dan perusakan harta benda.</li>
+              <li>Melaporkan kegiatan yang mencurigakan atau tindakan yang mengganggu ketertiban desa kepada pihak berwenang.</li>
+            </ul>
+          </li>
+          <li>
+            <h5 class="fw-semibold">Pengelolaan Lingkungan</h5>
+            <ul>
+              <li>Melakukan kegiatan penanaman pohon dan menjaga kebersihan lingkungan.</li>
+              <li>Menghindari tindakan yang dapat merusak lingkungan seperti illegal logging dan penangkapan ikan ilegal.</li>
+            </ul>
+          </li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Tata Tertib Desa -->
+
+<!-- Statistik Kependudukan -->
+<section id="statistik" class="statistik">
+  <div class="row justify-content-center">
+    <div class="col-md-8 text-center">
+      <h2>Statistik Kependudukan</h2>
+      <div class="col-lg-6 d-flex justify-content-between align-items-center w-100 h-100">
+        <span><i class="bi bi-people"></i><br />
+          <h2><?php echo $jumlahPenduduk; ?></h2>
+          <h5>Jumlah Penduduk</h5>
+        </span>
+        <span><i class="bi bi-person-vcard"></i><br />
+          <h2><?php echo $jumlahKepalaKeluarga ?></h2>
+          <h5>Kepala Keluarga</h5>
+        </span>
+        <span><i class="bi bi-gender-male"></i><br />
+          <h2><?php echo $jumlahPria ?></h2>
+          <h5>Laki-Laki</h5>
+        </span>
+        <span><i class="bi bi-gender-female"></i><br />
+          <h2><?php echo $jumlahWanita ?></h2>
+          <h5>Perempuan</h5>
+        </span>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Statistik Kependudukan -->
+<?php include 'templates/footer.php'; ?>
