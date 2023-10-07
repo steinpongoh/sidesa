87aa1713d0609ce3cca90da8137d1e7ef8460f28
trick2001 <willemlouis99@gmail.com>
2023-10-07 17:03:09 +0800

    menambahkan pengecekan di database

--- templates/dbconnect.php
+++ templates/dbconnect.php
@@ -1,3 +1,8 @@
 <?php
-$dbconnect = mysqli_connect("localhost","root","","desa");
-?>
+$dbconnect = mysqli_connect("localhost", "root", "", "desa");
+
+// Cek apakah ada error di database
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
