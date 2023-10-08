1ee95fe3d982c37344bf58e4e430e60ea4339a5f
trick2001 <willemlouis99@gmail.com>
2023-10-08 11:30:34 +0800

    menambahkan fitur galeri sementara

--- galeri.php
+++ galeri.php
@@ -1,3 +1,64 @@
 <?php
 
 include 'templates/header.php';
+
+?>
+
+<section id="gallery" class="gallery">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-10 text-center">
+                <h1>Galeri Desa</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, nisi.</p>
+            </div>
+
+            <div class="row row-cols-lg-4 row-cols-md-3 row-cols-sm-2 row-cols-1 justify-content-center">
+                <div class="col mt-3">
+                    <a href="img/galeri/desa1.jpg" data-toggle="lightbox" data-caption="This describes the image" data-gallery="mygallery">
+                        <img src="img/galeri/thumbnail/desa1.jpg" alt="desa1" class="img-fluid w-100 rounded">
+                    </a>
+                </div>
+                <div class="col mt-3">
+                    <a href="img/galeri/desa2.jpg" data-toggle="lightbox" data-caption="This describes the image" data-gallery="mygallery">
+                        <img src="img/galeri/thumbnail/desa2.jpg" alt="desa2" class="img-fluid w-100 rounded">
+                    </a>
+                </div>
+                <div class="col mt-3">
+                    <a href="img/galeri/desa3.jpg" data-toggle="lightbox" data-caption="This describes the image" data-gallery="mygallery">
+                        <img src="img/galeri/thumbnail/desa3.jpg" alt="desa3" class="img-fluid w-100 rounded">
+                    </a>
+                </div>
+                <div class="col mt-3">
+                    <a href="img/galeri/desa3.jpg" data-toggle="lightbox" data-caption="This describes the image" data-gallery="mygallery">
+                        <img src="img/galeri/thumbnail/desa3.jpg" alt="desa3" class="img-fluid w-100 rounded">
+                    </a>
+                </div>
+                <div class="col mt-3">
+                    <a href="img/galeri/desa3.jpg" data-toggle="lightbox" data-caption="This describes the image" data-gallery="mygallery">
+                        <img src="img/galeri/thumbnail/desa3.jpg" alt="desa3" class="img-fluid w-100 rounded">
+                    </a>
+                </div>
+                <div class="col mt-3">
+                    <a href="img/galeri/desa3.jpg" data-toggle="lightbox" data-caption="This describes the image" data-gallery="mygallery">
+                        <img src="img/galeri/thumbnail/desa3.jpg" alt="desa3" class="img-fluid w-100 rounded">
+                    </a>
+                </div>
+                <div class="col mt-3">
+                    <a href="img/galeri/desa3.jpg" data-toggle="lightbox" data-caption="This describes the image" data-gallery="mygallery">
+                        <img src="img/galeri/thumbnail/desa3.jpg" alt="desa3" class="img-fluid w-100 rounded">
+                    </a>
+                </div>
+                <div class="col mt-3">
+                    <a href="img/galeri/desa3.jpg" data-toggle="lightbox" data-caption="This describes the image" data-gallery="mygallery">
+                        <img src="img/galeri/thumbnail/desa3.jpg" alt="desa3" class="img-fluid w-100 rounded">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</section>
+<?php
+include 'templates/footer.php';
+?>
--- templates/footer.php
+++ templates/footer.php
@@ -20,7 +20,11 @@ include 'header.php';
     feather.replace();
 </script>
 
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+<!-- lightbox bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bs5-lightbox@1.8.3/dist/index.bundle.min.js"></script>
 </body>
 
 </html>
