277ab3548cb6746890d2bf2f4cab0d6eb9e8a8b1
trick2001 <willemlouis99@gmail.com>
2023-10-09 19:56:25 +0800

    memperbaiki styling

--- berita.php
+++ berita.php
@@ -6,19 +6,19 @@ include 'function.php';
 
 <section id="berita" class="berita">
   <div class="container">
-  <div class="container" style = "margin-top: 80px;">
-  <h1 class="text-center">BERITA</h1>
-  <h3 class="text-center">Berikut ini adalah berita seputar Desa Tondangow.</h3>
-  <div class="row g-0 bg-body-secondary position-relative">
-  <div class="col-md-6 mb-md-0 p-md-4">
-    <img src="img/galeri/desa1.jpg" class="w-100" alt="">
-  </div>
-  <div class="col-md-6 p-4 ps-md-0">
-    <h5 class="text-center mt-0">judul berita</h5>
-    <p class="d-block " style="max-width: ;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit tenetur inventore culpa sint praesentium id voluptatem nisi rerum laudantium! Cumque blanditiis animi quaerat eveniet reiciendis aliquid provident necessitatibus alias voluptatum!</p>
-    <a href="#" class="stretched-link">Selengkapnya</a>
-  </div>
-  </div>
-  </div>
+    <div class="container" style="margin-top: 80px;">
+      <h1 class="text-center">BERITA</h1>
+      <h3 class="text-center">Berikut ini adalah berita seputar Desa Tondangow.</h3>
+      <div class="row g-0 bg-body-secondary position-relative">
+        <div class="col-md-6 mb-md-0 p-md-4">
+          <img src="img/galeri/desa1.jpg" class="w-100" alt="">
+        </div>
+        <div class="col-md-6 p-4 ps-md-0">
+          <h5 class="text-center mt-0">judul berita</h5>
+          <p class="d-block " style="max-width: ;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit tenetur inventore culpa sint praesentium id voluptatem nisi rerum laudantium! Cumque blanditiis animi quaerat eveniet reiciendis aliquid provident necessitatibus alias voluptatum!</p>
+          <a href="#" class="stretched-link">Selengkapnya</a>
+        </div>
+      </div>
+    </div>
   </div>
 </section>
