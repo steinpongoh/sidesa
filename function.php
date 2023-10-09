6c20df0e157cd83804958bfc959c6f42a1a22dc4
Stein Pongoh <steinpongoh@gmail.com>
2023-10-09 12:40:51 +0800

    Da beking dpe bagian backend di pengaduan

--- function.php
+++ function.php
@@ -10,5 +10,22 @@
         }
         return $rows;
     }
+
+    function pengaduan($data, $file){
+        global $dbconnect;
+        $nama_pengadu=$data['nama'];
+        $alamat_lengkap=$data['alamat'];
+        $no_telepon=$data['no_telp'];
+        $judul_pengaduan=$data['judul'];
+        $tanggal_pengaduan=$data['tanggal'];
+        $isi_pengaduan=$data['detail'];
+
+        $direktori='img/pengaduan/';
+        $foto=$file['gambar']['name'];
+        move_uploaded_file($file['gambar']['tmp_name'],$direktori.$foto);
+        $queryAdd="INSERT INTO pengaduan VALUES('','$nama_pengadu','$alamat_lengkap','$judul_pengaduan','$isi_pengaduan','$tanggal_pengaduan','$no_telepon','$foto')";
+        mysqli_query($dbconnect, $queryAdd);
+        return mysqli_affected_rows($dbconnect);
+    }
     
 ?>
