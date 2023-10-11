88383b1467ae453827e3cebdc15e47e783a41d15
Valdi <rivaldisiwi@gmail.com>
2023-10-12 07:54:50 +0800

    menambahkan data database untuk berita

--- detailBerita.php
+++ detailBerita.php
@@ -1,18 +1,39 @@
 <?php
 include 'templates/header.php';
+
+$id=$_GET['id'];
+$query=mysqli_query($dbconnect,"select * from berita where id='$id'");
+$berita=mysqli_fetch_array($query);
+
+// membaca input dari form
+$input =$berita['detail_berita'];
+ 
+// memecah string input berdasarkan karakter '\r\n\r\n'
+$pecah = explode("\r\n\r\n", $input);
+ 
+// string kosong inisialisasi
+$text = " ";
+ 
+// untuk setiap substring hasil pecahan, sisipkan <p> di awal dan </p> di akhir
+// lalu menggabungnya menjadi satu string utuh $text
+for ($i=0; $i<=count($pecah)-1; $i++)
+{
+   $part = str_replace($pecah[$i], "<p>".$pecah[$i]."</p>", $pecah[$i]);
+   $text .= $part;
+}
 ?>
 
 <section class="detailBerita" id="detailBerita">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 text-center">
-                <h2>Judul Berita</h2>
-                <p class="text-start">Tanggal Berita</p>
+                <h2><?php echo $berita['judul_berita'];?></h2>
+                <p class="text-start"><?php echo $berita['tanggal_berita'];?></p>
                 <div class="col-md-4 img-detail">
                     <img src="img/sejarahdesa.jpg" class="mx-auto d-block" alt="">
                 </div>
                 <div class="col-md-8 justify-content-center w-100">
-                    <p class="isi-berita">Detail Berita <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut pariatur a, doloremque illum sint est vitae eum ducimus architecto corporis cumque enim, odio culpa necessitatibus deserunt eligendi. Aliquam neque vero assumenda at quaerat, suscipit, voluptates numquam, dicta explicabo sint voluptas inventore sapiente! Deleniti non eius ab sit, veritatis quasi dicta veniam laborum impedit alias rem architecto accusamus mollitia tempore libero voluptatem odit unde! Quia ullam corporis soluta sed molestias suscipit quidem aliquam esse distinctio. Assumenda magni, neque error eum totam veritatis cupiditate, sit magnam provident quidem pariatur eos tempore quae, quos nisi voluptatum! Aspernatur inventore voluptatem laudantium sapiente blanditiis? Officia minus mollitia aut quo dicta aspernatur. Ea error suscipit voluptatibus id, sunt et delectus quaerat iusto esse distinctio cum explicabo alias repellendus quo voluptates commodi ipsam nostrum atque repudiandae pariatur aliquid, perferendis eius, iure architecto! Velit qui aut molestias rem nesciunt optio aperiam eos deserunt odio culpa excepturi inventore, hic exercitationem ex. Earum beatae eum exercitationem dignissimos corrupti similique iste sint impedit blanditiis non, ipsa praesentium soluta eligendi corporis, rem dicta id hic, quod ullam commodi enim nam atque maxime. Ex natus quae, deleniti aspernatur pariatur consequuntur fuga tempore commodi, aliquid, omnis esse consectetur officiis dolorum eligendi ad porro expedita.
+                    <p class="isi-berita"><?php echo $berita['detail_berita']=$text;?>
                     </p>
                 </div>
             </div>
