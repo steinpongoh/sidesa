aaa75820aebf7b2aaf4452c4c462bad9a8f2954f
trick2001 <willemlouis99@gmail.com>
2023-10-12 00:40:00 +0800

    memperbaiki tampilan fitur galeri

--- galeri.php
+++ galeri.php
@@ -1,7 +1,7 @@
 <?php
 include 'templates/header.php';
 require 'function.php';
-$query=query("SELECT * FROM galeri")    ;
+$query = query("SELECT * FROM galeri");
 ?>
 
 <section id="gallery" class="gallery">
@@ -12,13 +12,13 @@ $query=query("SELECT * FROM galeri")    ;
                 <p>Galeri ini dibuat untuk mempromosikan desa, menampilkan budaya, tradisi, keindahan alam, dan kegiatan masyarakat Desa Tondangow.</p>
             </div>
             <div class="row row-cols-lg-4 row-cols-md-3 row-cols-sm-2 row-cols-1 justify-content-center" data-aos="zoom-in-right">
-                <?php foreach($query as $foto){?>
-                <div class="col mt-3">
-                    <a href=" img/galeri/<?= $foto['gambar']?>" data-toggle="lightbox" data-caption="<?=$foto['caption']?>" data-gallery="mygallery">
-                        <img src="img/galeri/<?= $foto['gambar']?>" alt="<?=$foto['caption']?>" class="img-fluid w-100 rounded">
-                    </a>
-                </div>
-                <?php }?>
+                <?php foreach ($query as $foto) { ?>
+                    <div class="col mt-3">
+                        <a href=" img/galeri/<?= $foto['gambar'] ?>" data-toggle="lightbox" data-caption="<?= $foto['caption'] ?>" data-gallery="mygallery">
+                            <img src="img/galeri/<?= $foto['gambar'] ?>" alt="<?= $foto['caption'] ?>" class="img-fluid w-100 rounded">
+                        </a>
+                    </div>
+                <?php } ?>
             </div>
         </div>
     </div>
