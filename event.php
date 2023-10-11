745165dcd68aa05e7b742adbd9932c964a60b00a
trick2001 <willemlouis99@gmail.com>
2023-10-11 09:02:30 +0800

    menambahkan fitur event desa

---
+++ event.php
@@ -0,0 +1,52 @@
+<?php
+include 'templates/header.php';
+?>
+
+
+<section class="event" id="event">
+    <div class="container">
+        <h1 class="text-center">Event Desa</h1>
+        <p class="text-center">Sistem informasi Desa Tondangow menyediakan informasi-informasi terkait Event/Acara yang ada di Desa Tondangow</p>
+        <div class="row row-cols-1 row-cols-md-2 g-5 py-8">
+            <div class="col">
+                <a href="" class="text-decoration-none fs-5">
+                    <div class="card">
+                        <img src="img/event/1.jpg" class="card-img-top" alt="..." />
+                        <div class="mb-5 d-flex justify-content-around">
+                            <p>Nama Event</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="" class="text-decoration-none fs-5">
+                    <div class="card">
+                        <img src="img/event/2.jpg" class="card-img-top" alt="..." />
+                        <div class="mb-5 d-flex justify-content-around">
+                            <p>Nama Event</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img/event/3.jpg" class="card-img-top" alt="..." />
+                    <div class="mb-5 d-flex justify-content-around">
+                        <button class="btn btn-primary">Nama Event</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img/event/1.jpg" class="card-img-top" alt="..." />
+                    <div class="mb-5 d-flex justify-content-around">
+                        <button class="btn btn-primary">Nama Event</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+include 'templates/footer.php';
+?>
