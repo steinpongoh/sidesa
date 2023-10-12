38ed58a54b2d602f25c727358f2899a790411ef7
trick2001 <willemlouis99@gmail.com>
2023-10-12 09:14:40 +0800

    solve masalah di detailberita

--- detailBerita.php
+++ detailBerita.php
@@ -1,25 +1,31 @@
 <?php
 include 'templates/header.php';
 
-$id=$_GET['id'];
-$query=mysqli_query($dbconnect,"select * from berita where id='$id'");
-$berita=mysqli_fetch_array($query);
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
 
-// membaca input dari form
-$input =$berita['detail_berita'];
- 
-// memecah string input berdasarkan karakter '\r\n\r\n'
-$pecah = explode("\r\n\r\n", $input);
- 
-// string kosong inisialisasi
-$text = " ";
- 
-// untuk setiap substring hasil pecahan, sisipkan <p> di awal dan </p> di akhir
-// lalu menggabungnya menjadi satu string utuh $text
-for ($i=0; $i<=count($pecah)-1; $i++)
-{
-   $part = str_replace($pecah[$i], "<p>".$pecah[$i]."</p>", $pecah[$i]);
-   $text .= $part;
+    // Gunakan prepared statement untuk mencegah SQL injection
+    $query = mysqli_prepare($dbconnect, "SELECT * FROM berita WHERE id = ?");
+    mysqli_stmt_bind_param($query, 'i', $id);
+    mysqli_stmt_execute($query);
+    $result = mysqli_stmt_get_result($query);
+
+    if ($result && $berita = mysqli_fetch_array($result)) {
+        $input = $berita['detail_berita'];
+        $pecah = explode("\r\n\r\n", $input);
+
+        $text = '';
+
+        for ($i = 0; $i < count($pecah); $i++) {
+            $text .= "<p>{$pecah[$i]}</p>";
+        }
+    } else {
+        echo "Berita tidak ditemukan.";
+        exit();
+    }
+} else {
+    echo "ID berita tidak valid.";
+    exit();
 }
 ?>
 
@@ -27,18 +33,19 @@ for ($i=0; $i<=count($pecah)-1; $i++)
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 text-center">
-                <h2><?php echo $berita['judul_berita'];?></h2>
-                <p class="text-start"><?php echo $berita['tanggal_berita'];?></p>
+                <h2><?php echo $berita['judul_berita']; ?></h2>
+                <p class="text-start"><?php echo $berita['tanggal_berita']; ?></p>
                 <div class="col-md-4 img-detail">
                     <img src="img/galeri/<?= $berita['gambar'] ?>" class="mx-auto d-block" alt="">
                 </div>
-                <div class="col-md-8 justify-content-center w-100">
-                    <p class="isi-berita"><?php echo $berita['detail_berita']=$text;?></p>  
+                <div class="col-md-8 justify-content-center w-100 isi-berita">
+                    <?php echo $text; ?>
                 </div>
             </div>
         </div>
     </div>
 </section>
+
 <?php
 include 'templates/footer.php';
 ?>
