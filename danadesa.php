42612f0eca14fa8c04943a592db8a25a31e471da
trick2001 <willemlouis99@gmail.com>
2023-10-18 11:14:52 +0800

    menambahkan fitur dana desa

--- danadesa.php
+++ danadesa.php
@@ -4,7 +4,39 @@ include 'templates/header.php';
 
 
 
-
+<section class="danadesa" id="danadesa">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-9 text-center mb-3">
+                <h1>Dana Desa</h1>
+                <p>Dana Desa adalah dana yang bersumber dari Anggaran Pendapatan dan Belanja Negara yang diperuntukkan bagi desa yang ditransfer melalui APBD kabupaten/kota dan digunakan untuk membiayai penyelenggaraan pemerintahan, pelaksanaan pembangunan, pembinaan kemasyarakatan, dan pemberdayaan masyarakat.</p>
+            </div>
+            <div class="col-md-10 text-center">
+                <table class="table table-bordered border-black">
+                    <thead>
+                        <tr class="table-dark">
+                            <th scope="col">#</th>
+                            <th scope="col">Informasi</th>
+                            <th scope="col">Deskripsi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto< /td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
