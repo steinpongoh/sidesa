16c8a5cf7cdb38201c41dad07fbdd79d2dd924a1
trick2001 <willemlouis99@gmail.com>
2023-10-22 15:19:01 +0800

    memperbaiki style tabel

--- danadesa.php
+++ danadesa.php
@@ -6,15 +6,15 @@ include 'templates/header.php';
 
 <section class="danadesa" id="danadesa">
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center" data-aos="fade-down-left">
             <div class="col-md-9 text-center mb-3">
                 <h1>Dana Desa</h1>
                 <p>Dana Desa adalah dana yang bersumber dari Anggaran Pendapatan dan Belanja Negara yang diperuntukkan bagi desa yang ditransfer melalui APBD kabupaten/kota dan digunakan untuk membiayai penyelenggaraan pemerintahan, pelaksanaan pembangunan, pembinaan kemasyarakatan, dan pemberdayaan masyarakat.</p>
             </div>
-            <div class="col-md-10 text-center">
-                <table class="table table-bordered border-success">
-                    <thead>
-                        <tr class="table-success table-bordered border-success">
+            <div class="col-md-10 text-center table-responsive">
+                <table class="table table-bordered border-success" cellspacing="0">
+                    <thead class="table-success table-bordered border-success align-middle">
+                        <tr>
                             <th scope="col">#</th>
                             <th scope="col">Informasi</th>
                             <th scope="col">Deskripsi</th>
--- list_pengaduan.php
+++ list_pengaduan.php
@@ -7,49 +7,54 @@ $queryPengaduan = query('SELECT * FROM pengaduan');
 ?>
 
 <section id="pengaduan" class="pengaduan">
-  <div class="container">
-    <div class="row justify-content-center" data-aos="fade-down-left">
-        <h1 class="text-center">Pengaduan Masyarakat</h1>
-        <p class="text-center">Sistem Informasi Desa Tondangow menyediakan forum pengaduan masyarakat, apabila ada masyarakat yang ingin melakukan pengaduan</p>
-        <div class="shadow p-5 mt-4 mb-4 rounded-2 border-top border-success border-3 bg-light">
-        <a href="pengaduan.php" class="float-end btn btn-danger">Buat Pengaduan
-        </a><br>
-        <br>
-        <table class="table table-bordered border-primary" id="dataTable" width="100%" cellspacing="0">
-            <thead class="table-dark border-primary">
-                <tr>
-                    <th>No</th>
-                    <th>Nama Lengkap</th>
-                    <th>Alamat</th>
-                    <th>Foto</th>
-                    <th>Judul Pengaduan</th>
-                    <th>Tanggal Pengaduan</th>
-                    <th>Detail Pengaduan</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php $i = 1 ?>
-                <?php foreach ($queryPengaduan as $rows) { ?>
-                    <tr>
-                        <td><?= $i ?></td>
-                        <td><?php echo $rows['nama_pengadu'] ?></td>
-                        <td><?php echo $rows['alamat_lengkap'] ?></td>
-                        <td>
-                            <img width="60px" height="60px" src="img/pengaduan/<?php echo $rows['gambar']?>" alt="">
-                        </td>
-                        <td><?php echo $rows['judul_pengaduan']?></td>
-                        <td><?php echo date('d M Y H:i', strtotime($rows['tanggal_pengaduan']))?></td>
-                        <td><?php echo $rows['detail_pengaduan']?></td>
-                        <td></td>
-                    </tr>
-                <?php $i++ ?>
-                <?php } ?>
-            </tbody>
-        </table>
+    <div class="container">
+        <div class="row justify-content-center" data-aos="fade-down-left">
+            <div class="col-md-9 text-center mb-3">
+                <h1 class="text-center">Pengaduan Masyarakat</h1>
+                <p class="text-center">Sistem Informasi Desa Tondangow menyediakan forum pengaduan masyarakat, apabila ada masyarakat yang ingin melakukan pengaduan</p>
+            </div>
+            <div class="col-md-12 mb-4 float-end">
+                <a href="pengaduan.php" class="float-end btn btn-danger">Buat Pengaduan
+                </a>
+            </div>
+
+            <div class="col-md-12 text-center table-responsive overflow-x-auto">
+
+                <table class="table table-bordered border-success" id="dataTable" width="100%" cellspacing="0">
+                    <thead class="table-success table-bordered border-success align-middle">
+                        <tr>
+                            <th>No</th>
+                            <th>Nama Lengkap</th>
+                            <th>Alamat</th>
+                            <th>Foto</th>
+                            <th>Judul Pengaduan</th>
+                            <th>Tanggal Pengaduan</th>
+                            <th>Detail Pengaduan</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody class="align-middle">
+                        <?php $i = 1 ?>
+                        <?php foreach ($queryPengaduan as $rows) { ?>
+                            <tr>
+                                <td><?= $i ?></td>
+                                <td><?= $rows['nama_pengadu'] ?></td>
+                                <td><?= $rows['alamat_lengkap'] ?></td>
+                                <td>
+                                    <img width="60px" height="60px" src="img/pengaduan/<?= $rows['gambar'] ?>" alt="">
+                                </td>
+                                <td><?= $rows['judul_pengaduan'] ?></td>
+                                <td><?= date('d M Y H:i', strtotime($rows['tanggal_pengaduan'])) ?></td>
+                                <td><?= $rows['detail_pengaduan'] ?></td>
+                                <td></td>
+                            </tr>
+                            <?php $i++ ?>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-  </div>
 
 </section>
 
